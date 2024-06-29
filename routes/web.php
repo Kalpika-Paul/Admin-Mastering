91117dbb01cafda05160571136768cf17b9b7f3d
Kalpika-Paul <134072352+Kalpika-Paul@users.noreply.github.com>
2024-06-29 20:18:33 +0600

    initial commit

---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+
+
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\LoginController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::get ('/account/login', [LoginController::class,'index'])->name('account.login');
+Route::get ('/account/logout', [LoginController::class,'logout'])->name('account.logout');
+Route::post ('/account/authenticate', [LoginController::class,'authenticate'])->name('account.authenticate');
+Route::post ('/account/process-register', [LoginController::class,'processRegister'])->name('account.processRegister');
+
+Route::get ('/account/dashboard', [DashboardController::class,'index'])->name('account.dashboard');
+Route::get('/account/register', [LoginController::class,'register'])->name('account.register');
+
+Route::get('/account/categories', [DashboardController::class,'categories'])->name('account.categories');
+
+
