e285932443a5628652d4868eb61c4136e072de8e
Kalpika-Paul <134072352+Kalpika-Paul@users.noreply.github.com>
2024-06-30 02:01:42 +0600

    Your commit message

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Models\Category;
 
 class DashboardController extends Controller
 {
@@ -11,14 +11,40 @@ class DashboardController extends Controller
         return view ('dashboard');
     }
 
-    public function categories(){
-        return view ('categories');
+    public function category(){
+        return view ('category');
     }
 
     public function newcategory (){
         return view ('newcategory');
     }
+   
+    public function store (Request $request)
+    {
+        // dd($request->name);
+
+        $data = $request->validate([
+           
+            'name'=>'required',
+            'address'=>'required',
+            'phone'=>'required',
+            'email'=>'required|email',
+            'area'=>'required'
 
 
+        ]);
+
+
+         $newCategory = Category::create($data);
+     
+         if ($newCategory) {
+        return redirect(route('account.dashboard'))->with('success', 'Category created successfully');
+    }   else {
+        return redirect()->back()->with('error', 'Failed to create category');
+    }
+       
+    }
+
+   
 
 }
--- database/migrations/2024_06_29_165418_create_category_table.php
+++ database/migrations/2024_06_29_165418_create_category_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('address');
-            $table->integer('phone');
+            $table->string('phone');
             $table->string('email');
             $table->string('area');
             $table->timestamps('');
